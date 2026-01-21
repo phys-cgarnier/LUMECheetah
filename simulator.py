5687dd5186f880618b6dc493875d29e728bbb74f
Chris Garnier <cgarnier@slac.stanford.edu>
2026-01-20 16:03:15 -0800

    created methods for simulator tracking, resetting, and setting up lattice

--- simulator.py
+++ simulator.py
@@ -14,15 +14,25 @@ class CheetahSimulator():
         self.subcell_dest = subcell_dest
         self.initial_beam_distribution = initial_particle_distribution
         self.beam_distribution = initial_particle_distribution
+
+        self.setup_lattice()
+        self.track()
+
+    #TODO: what does lattice do with particle beam other than track.
+    def setup_lattice(self):
         self.lattice = Segment.from_lattice_json(self.lattice_file)
+        for ele in self.lattice.elements:
+            if isinstance(ele, Screen):
+                ele.method = "histogram"
+        
         if self.subcell_dest:
             self.lattice = self.lattice.subcell(end=self.subcell_dest)
 
-    #TODO: what does lattice do with particle beam other than track.
     def reset(self):
-        pass
+        self.setup_lattice()
+        self.beam_distribution = self.initial_beam_distribution
 
     def track(self):
-        pass
-
+        self.lattice.track(self.beam_distribution)
     
+
